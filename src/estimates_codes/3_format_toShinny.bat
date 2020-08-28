8a869da01045ff06c3e7ee21562bd03ae6b93aa8
Vinícius Mesquita <vieiramesquita@gmail.com>
2020-08-28 09:47:01 -0300

    Create 3_format_toShinny.bat

---
+++ src/estimates_codes/3_format_toShinny.bat
@@ -0,0 +1,27 @@
+mkdir lv1
+mkdir lv2
+mkdir lv3
+
+copy l1\acc_int_AMAZÔNIA.csv lv1\acc_int_Amazonia.csv
+copy l1\acc_int_CAATINGA.csv lv1\acc_int_Caatinga.csv 
+copy l1\acc_int_CERRADO.csv lv1\acc_int_Cerrado.csv
+copy l1\acc_int_BRASIL.csv lv1\acc_int_Global.csv
+copy l1\"acc_int_MATA ATLÂNTICA.csv" lv1\"acc_int_Mata Atlantica.csv"
+copy l1\acc_int_PAMPA.csv lv1\acc_int_Pampa.csv
+copy l1\acc_int_PANTANAL.csv lv1\acc_int_Pantanal.csv
+
+copy l2\acc_int_AMAZÔNIA.csv lv2\acc_int_Amazonia.csv
+copy l2\acc_int_CAATINGA.csv lv2\acc_int_Caatinga.csv 
+copy l2\acc_int_CERRADO.csv lv2\acc_int_Cerrado.csv
+copy l2\acc_int_BRASIL.csv lv2\acc_int_Global.csv
+copy l2\"acc_int_MATA ATLÂNTICA.csv" lv2\"acc_int_Mata Atlantica.csv"
+copy l2\acc_int_PAMPA.csv lv2\acc_int_Pampa.csv
+copy l2\acc_int_PANTANAL.csv lv2\acc_int_Pantanal.csv
+
+copy l3\acc_int_AMAZÔNIA.csv lv3\acc_int_Amazonia.csv
+copy l3\acc_int_CAATINGA.csv lv3\acc_int_Caatinga.csv 
+copy l3\acc_int_CERRADO.csv lv3\acc_int_Cerrado.csv
+copy l3\acc_int_BRASIL.csv lv3\acc_int_Global.csv
+copy l3\"acc_int_MATA ATLÂNTICA.csv" lv3\"acc_int_Mata Atlantica.csv"
+copy l3\acc_int_PAMPA.csv lv3\acc_int_Pampa.csv
+copy l3\acc_int_PANTANAL.csv lv3\acc_int_Pantanal.csv
