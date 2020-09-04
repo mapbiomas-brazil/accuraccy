de0eb568b0e280ea3a2776f7725b868d181b0c3d
Vinícius Mesquita <vieiramesquita@gmail.com>
2020-09-04 16:09:15 -0300

    Add files via upload

---
+++ src/estimates_codes/replace.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+mkdir -p lv1
+mkdir -p lv2
+mkdir -p lv3
+
+cp -v l1/acc_int_AMAZÔNIA.csv lv1/acc_int_Amazonia.csv
+cp -v l1/acc_int_CAATINGA.csv lv1/acc_int_Caatinga.csv 
+cp -v l1/acc_int_CERRADO.csv lv1/acc_int_Cerrado.csv
+cp -v l1/acc_int_BRASIL.csv lv1/acc_int_Global.csv
+cp -v l1/'acc_int_MATA ATLÂNTICA.csv' lv1/'acc_int_Mata Atlantica.csv'
+cp -v l1/acc_int_PAMPA.csv lv1/acc_int_Pampa.csv
+cp -v l1/acc_int_PANTANAL.csv lv1/acc_int_Pantanal.csv
+
+cp -v l2/acc_int_AMAZÔNIA.csv lv2/acc_int_Amazonia.csv
+cp -v l2/acc_int_CAATINGA.csv lv2/acc_int_Caatinga.csv 
+cp -v l2/acc_int_CERRADO.csv lv2/acc_int_Cerrado.csv
+cp -v l2/acc_int_BRASIL.csv lv2/acc_int_Global.csv
+cp -v l2/'acc_int_MATA ATLÂNTICA.csv' lv2/'acc_int_Mata Atlantica.csv'
+cp -v l2/acc_int_PAMPA.csv lv2/acc_int_Pampa.csv
+cp -v l2/acc_int_PANTANAL.csv lv2/acc_int_Pantanal.csv
+
+cp -v l3/acc_int_AMAZÔNIA.csv lv3/acc_int_Amazonia.csv
+cp -v l3/acc_int_CAATINGA.csv lv3/acc_int_Caatinga.csv 
+cp -v l3/acc_int_CERRADO.csv lv3/acc_int_Cerrado.csv
+cp -v l3/acc_int_BRASIL.csv lv3/acc_int_Global.csv
+cp -v l3/'acc_int_MATA ATLÂNTICA.csv' lv3/'acc_int_Mata Atlantica.csv'
+cp -v l3/acc_int_PAMPA.csv lv3/acc_int_Pampa.csv
+cp -v l3/acc_int_PANTANAL.csv lv3/acc_int_Pantanal.csv
